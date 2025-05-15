1f80b648b40be56500685e0c416b51306a985f99
a12355765 <a12355765@gmail.com>
2025-05-16 01:23:07 +0800

    google cloud run

--- app.py
+++ app.py
@@ -393,5 +393,5 @@ def analyze_trash():
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
